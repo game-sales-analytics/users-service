4765caf9ef9a8e7b72f30c4d910dff61c58660e2
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-12 14:29:20 +0330

    fix: add error handling to auth service

--- internal/auth/login.go
+++ internal/auth/login.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/game-sales-analytics/users-service/internal/db/repository"
+	"github.com/game-sales-analytics/users-service/internal/id"
 	"github.com/game-sales-analytics/users-service/internal/passhash"
 )
 
@@ -26,38 +27,51 @@ func (a authsrv) LoginWithEmail(ctx context.Context, creds LoginWithEmailCreds) 
 	if nil != err {
 		if errors.Is(err, repository.ErrUserNotExists) {
 			randSpan := rand.Int63n(251)
-			time.Sleep(time.Millisecond * time.Duration((4736 + randSpan)))
-			return nil, errors.New("unauthorized")
+			time.Sleep(time.Millisecond * time.Duration((6136 + randSpan)))
+			return nil, ErrUnauthenticated
 		}
 
-		return nil, errors.New("internal")
+		a.logger.WithError(err).WithField("err_code", "E_RETRIEVE_USER_LOGIN_INFO").Error("failed retrieving user login information")
+		return nil, ErrInternal
 	}
 
 	matched, err := passhash.Verify(creds.Password, user.Password)
 	if nil != err {
-		return nil, errors.New("internal")
+		a.logger.WithError(err).WithField("err_code", "E_VERIFY_PASSWORD").Error("failed verifying user password for login")
+		return nil, ErrInternal
 	}
 	if !matched {
-		return nil, errors.New("unauthorized")
+		return nil, ErrUnauthenticated
 	}
 
-	token, err := generateToken(user.ID, a.cfg.Secret)
+	token, err := a.generateToken(user.ID, a.cfg.Secret)
 	if nil != err {
-		return nil, errors.New("internal")
+		a.logger.WithError(err).WithField("err_code", "E_GENERATE_AUTH_TOKEN").Error("failed generating authentication token for user")
+		return nil, ErrInternal
 	}
 
+	loginRecordID, err := id.GenerateUserLoginID()
+	if nil != err {
+		a.logger.WithError(err).WithField("err_code", "E_GENERATE_NEW_LOGIN_RECORD_ID").Error("failed generating id for new user login record")
+	}
 	loginRecord := repository.NewUserLoginToSave{
-		ID:                  "",
+		ID:                  loginRecordID,
 		UserID:              user.ID,
 		LoggedInAt:          time.Now(),
 		UserIPAddress:       creds.UserIPAddress,
 		UserDeviceUserAgent: creds.UserDeviceUserAgent,
 	}
 	if err := a.repo.SaveNewUserLogin(ctx, loginRecord); nil != err {
+		a.logger.WithError(err).WithField("err_code", "E_SAVE_NEW_LOGIN_INFO").Error("failed saving user new login attempt information")
 		return nil, err
 	}
 
 	return &LoginResult{
-		Token: token,
+		Token: LoginResultToken{
+			ID:                 token.ID,
+			Value:              token.Value,
+			NotBeforeDateTime:  token.NotBeforeDateTime,
+			ExpirationDateTime: token.ExpirationDateTime,
+		},
 	}, nil
 }
