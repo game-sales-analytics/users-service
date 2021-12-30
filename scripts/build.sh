45a2c271220c5c0827ac00e03fb6b6589e6aa7c3
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-30 12:14:51 +0330

    build: prevent unnecessary variable export in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,6 @@ set -ex
 rm -vrf ./bin
 mkdir -vp ./bin
 PKGS=$(go list -find -f '{{.ImportPath}}' ./cmd/...)
-export PKGS
 for pkg in $PKGS; do
   echo "building package $pkg... "
   go build -o ./bin/ "$pkg"
